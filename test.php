9821953ec487558d3137daf352b369b3ca307094
Imran <imranmajeed@outlook.com>
2020-06-16 18:38:26 +0500

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,5 @@
+<php
   $ytdata = file_get_contents("https://www.youtube.com/oembed?format=json&url=https://www.youtube.com/watch?v=MjxGwfa5lxw");
   $ytjson = json_decode($ytdata);
   $thumbnailimageurl = $ytjson->thumbnail_url;
+?>
